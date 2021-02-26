c41abbfa60aa7478b239dfe6196b8f457aaf4d87
Guilherme <guilgchaves@protonmail.com>
2021-02-26 19:33:15 -0300

    Docs(blurry-loading): script comments added for scale function;

--- blurry-loading-page/script.js
+++ blurry-loading-page/script.js
@@ -17,6 +17,7 @@ function blurring() {
   bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
 }
 
+// this function syncs the loadText time with the bg blurrying time as found in a stackoverflow thread.
 // https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
 const scale = (num, in_min, in_max, out_min, out_max) => {
   return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
